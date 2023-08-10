9d478a615c127bca040330575512c9b6dec830bc
Sandhiya <sandhiya.g@knackforge.com>
2023-08-10 16:19:47 +0530

    Console added Co-authored-by: Johnson <johnsondevaasir07@gmail.com>

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ app.get('/',(req: Request, res: Response) => {
   res.send('Express + Mongo CRUD with Typescript');
 });
 app.listen(config.PORT, () => {
-  console.log( `Server running in http://localhost:${config.PORT}/`);
+  console.log( `Server running on http://localhost:${config.PORT}/`);
 });
 
 //Declare routes
